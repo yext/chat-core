81ed42bc5f84c4ac12ce2844e388befc4a55d393
Yen Truong <ytruong@yext.com>
2023-05-10 13:26:24 -0400

    source as enum or string literal

--- src/models/index.ts
+++ src/models/index.ts
@@ -4,3 +4,5 @@ export { Message, MessageSource } from './endpoints/Message';
 export { MessageNotes } from './endpoints/MessageNotes';
 export { MessageRequest } from './endpoints/MessageRequest';
 export { MessageResponse } from './endpoints/MessageResponse';
+
+export { EnumOrLiteral } from './utils/EnumOrLiteral';
--- tests/ChatCore.test.ts
+++ tests/ChatCore.test.ts
@@ -1,4 +1,4 @@
-import { MessageRequest, MessageSource } from '../src';
+import { MessageRequest, MessageResponse, MessageSource } from '../src';
 import { ChatCore } from '../src/ChatCore';
 import { defaultApiVersion } from '../src/constants';
 import { HttpService } from '../src/http/HttpService';
@@ -6,6 +6,7 @@ import { HttpService } from '../src/http/HttpService';
 const emptyMessageRequest: MessageRequest = {
   messages: []
 };
+
 it('sets default api domain and businessId when not specified', async () => {
   const httpServiceSpy = jest.spyOn(HttpService.prototype, 'post')
     .mockResolvedValue({
@@ -48,7 +49,7 @@ it('sets custom api domain, businessId, version when specified', async () => {
 });
 
 it('returns message response on successful API response', async () => {
-  const expectedMessageResponse = {
+  const expectedMessageResponse: MessageResponse = {
     message: {
       text: 'hello world!',
       source: MessageSource.BOT,
