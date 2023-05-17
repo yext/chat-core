e3229c6081a3321498fbf7dc995296add030378a
Yen Truong <ytruong@yext.com>
2023-05-17 10:15:23 -0400

    reword

--- docs/chat-core.messagerequest.context.md
+++ docs/chat-core.messagerequest.context.md
@@ -4,7 +4,7 @@
 
 ## MessageRequest.context property
 
-Additional information to pass into the instruction flow. This data could then be use in the url or body of a REST API step, influence Chat API's assessment in a conditional step, or help construct a reply with additional details.
+Additional information to pass into the instruction flow. This data could then be used in the URL or body of a REST API step, influence Chat API's assessment in a conditional step, or help construct a reply with additional details.
 
 **Signature:**
 
--- src/models/endpoints/MessageRequest.ts
+++ src/models/endpoints/MessageRequest.ts
@@ -29,9 +29,10 @@ export interface MessageRequest {
   /** {@inheritDoc MessageNotes} */
   notes?: MessageNotes;
   /**
-   * Additional information to pass into the instruction flow. This data could then be
-   * use in the url or body of a REST API step, influence Chat API's assessment
-   * in a conditional step, or help construct a reply with additional details.
+   * Additional information to pass into the instruction flow. This data could
+   * then be used in the URL or body of a REST API step, influence Chat API's
+   * assessment in a conditional step, or help construct a reply with additional
+   * details.
    *
    * @remarks
    * May be any valid JSON object
