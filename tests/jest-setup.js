ee43654d77decc6eb0a82cf5977230b9aac48c57
Yen Truong <ytruong@yext.com>
2023-05-22 17:52:33 -0400

    bump version 0.3.0

--- tests/jest-setup.js
+++ tests/jest-setup.js
@@ -2,7 +2,7 @@ import { TextDecoder, TextEncoder } from "util";
 
 /**
  * jest's jsdom doesn't have the following properties defined in global for the DOM.
- * polyfill it with decoder from NodeJS
+ * polyfill it with functions from NodeJS
  */
 global.TextDecoder = TextDecoder;
 global.TextEncoder = TextEncoder;
