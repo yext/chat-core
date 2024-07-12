785a2979695748b51cc756a82b9fe990ffcc0c39
Yen Truong <ytruong@yext.com>
2024-07-12 13:39:24 -0400

    different temp folder per package build

--- generate-notices.sh
+++ generate-notices.sh
@@ -7,16 +7,17 @@
 
 
 PACKAGE_DIR=$(pwd)
+PACKAGE_NAME=$(basename "$(pwd)")
 REPO_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
-echo "copying package $PACKAGE_DIR to $REPO_DIR/temp"
-rm -rf "$REPO_DIR/temp"
-# cp -r $PACKAGE_DIR "$REPO_DIR/temp"
+TEMP_DIR="$REPO_DIR/temp-$PACKAGE_NAME"
 
-rsync -av --exclude=node_modules --exclude=test-browser-esm --exclude=test-node-cjs "$PACKAGE_DIR/" "$REPO_DIR/temp/"
-
-cd "$REPO_DIR/temp"
+echo "copying package $PACKAGE_DIR to $TEMP_DIR"
+rm -rf $TEMP_DIR
+# TODO: omit exclude test folders when it's moved to the root of the repo
+rsync -av --exclude=node_modules --exclude=test-browser-esm --exclude=test-node-cjs "$PACKAGE_DIR/" "$TEMP_DIR/"
+cd $TEMP_DIR
 rm -rf node_modules
 npm i --ignore-scripts
 generate-license-file --input package.json --output THIRD-PARTY-NOTICES --overwrite
-cp "$REPO_DIR/temp/THIRD-PARTY-NOTICES" $PACKAGE_DIR
-rm -rf "$REPO_DIR/temp"
+cp "$TEMP_DIR/THIRD-PARTY-NOTICES" $PACKAGE_DIR
+rm -rf $TEMP_DIR
