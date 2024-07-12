88ea92d95e1855996033b63b15744608639ad94e
Yen Truong <ytruong@yext.com>
2024-07-12 13:20:53 -0400

    remove test sites during copy

--- generate-notices.sh
+++ generate-notices.sh
@@ -10,8 +10,13 @@ PACKAGE_DIR=$(pwd)
 REPO_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
 echo "copying package $PACKAGE_DIR to $REPO_DIR/temp"
 rm -rf "$REPO_DIR/temp"
-cp -r $PACKAGE_DIR "$REPO_DIR/temp"
-cd "$REPO_DIR/temp" && npm i --ignore-scripts
+# cp -r $PACKAGE_DIR "$REPO_DIR/temp"
+
+rsync -av --exclude=node_modules --exclude=test-browser-esm --exclude=test-node-cjs "$PACKAGE_DIR/" "$REPO_DIR/temp/"
+
+cd "$REPO_DIR/temp"
+rm -rf node_modules
+npm i --ignore-scripts
 generate-license-file --input package.json --output THIRD-PARTY-NOTICES --overwrite
 cp "$REPO_DIR/temp/THIRD-PARTY-NOTICES" $PACKAGE_DIR
 rm -rf "$REPO_DIR/temp"
